355aac00f771a2802485494b8e3b227716be874d
Xu Shiyan <xushiyan.1990@gmail.com>
2016-06-06 12:53:26 +0800

    p4 q1 random action

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -25,7 +25,7 @@ class LearningAgent(Agent):
         # TODO: Update state
         
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice((None, 'forward', 'left', 'right')) # random action
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -41,7 +41,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
