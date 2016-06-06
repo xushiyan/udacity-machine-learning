e6bf331521209b687ab6f8080c0366f5d0e3d5cc
Xu, Raymond <raymond.xu@sap.com>
2016-06-06 17:55:20 +0800

    update starter code using download zip

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -41,15 +41,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
-
-    sim.run(n_trials=100)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=10)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
